d0e8562267ed03c7ff4bea12858d6d1f8e413d9e
Truls Stenrud <truls.stenrud@gmail.com>
2017-01-26 13:44:30 +0100

    Client work

--- sMessage/src/model/Client.java
+++ sMessage/src/model/Client.java
@@ -1,8 +1,24 @@
 package model;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
+
 /**
  * @author s305089
  */
 public class Client {
+
+    Socket clientsocket;
+    DataOutputStream outToServer;
+    BufferedReader inFromServer;
+    private InetAddress ip;
+    private int portNr;
+
+
+    
+
 
 }
