28bd0927f8be800420fbbcc29d642744de3191d7
Truls Stenrud <truls.stenrud@gmail.com>
2017-02-08 12:13:04 +0100

    Working on parser in Server

--- sMessage/src/model/Command.java
+++ sMessage/src/model/Command.java
@@ -4,12 +4,15 @@ package model;
  * @author s305046, s305080, s305084, s305089
  */
 public enum Command {
-    CONNECT,
-    DISCONNECT,
-    GETUSERS,
-    REGUSER,
-    LOGIN,
-    LOGINFAIL,
-    LOGINSUCCESS,
-    ERROR
+    CONNECT,        // , userID
+    REQUEST,        // , userID
+    RESPONSE,       // , "yes"/"no", userID
+    DISCONNECT,     // , userID
+    GETUSERS,       // ,
+    USERLIST,       //TODO
+    REGUSER,        // , uname, encryptedPassWord
+    LOGIN,          // , uname, encryptedPassWord
+    LOGINFAIL,      // ,
+    LOGINSUCCESS,   // ,
+    ERROR           //
 }
