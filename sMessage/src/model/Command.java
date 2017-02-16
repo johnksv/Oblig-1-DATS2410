f9647adb519f7afed45cbd9ca8082e7c2ed5247d
Stian Stensli <stian.hs@hotmail.com>
2017-02-16 10:49:50 +0100

    farger og javadoc command

--- sMessage/src/model/Command.java
+++ sMessage/src/model/Command.java
@@ -1,6 +1,8 @@
 package model;
 
 /**
+ * List of legal commands in Simple Messaging Protocol SMP.
+ * For information regarding each command read the "readme" file.
  * @author s305046, s305080, s305084, s305089
  */
 public enum Command {
