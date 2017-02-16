d9b26d9b4ea20ea3501b48dc0f478c2d1226c2e8
Stian Stensli <stian.hs@hotmail.com>
2017-02-16 10:13:54 +0100

    javadoc User

--- sMessage/src/model/User.java
+++ sMessage/src/model/User.java
@@ -1,6 +1,11 @@
 package model;
 
 /**
+ * A class dedicated to safly store passwords username and the status of users.
+ * Status is online(True) and offline (false).
+ * Busy is busy (true) and not busy (false) and can never be busy if not online.
+ * Password is stored as a encrypted string so you cant get the original clear text password from this class.
+ * 
  * @author s305046, s305080, s305084, s305089
  */
 public class User {
@@ -8,33 +13,64 @@ public class User {
     private final String uname, pswd;
     private boolean status;
     private boolean busy;
-
+    
+    /**
+    * Initiates a new user, and stores the password and username.
+    * Status is set here, but can be change later not like password and username.
+    */
     public User(String uname, String pswd, boolean status) {
 	this.uname = uname;
 	this.pswd = pswd;
 	this.status = status;
     }
-
+    
+    /**
+     * Returns username
+     * @return uname
+     */
     public String getUname() {
 	return uname;
     }
-
+    
+    /**
+     * Sets status to false.
+     * Sets busy to false.
+     */
     public void logOff() {
 	status = false;
         busy = false;
     }
-
+    
+    /**
+     * Returns true if online.
+     * @return status
+     */
     public boolean isOnline() {
 	return status;
     }
+    
+    /**
+     * Sets busy true or false.
+     * @param busy 
+     */
     public void setBusy(boolean busy) {
         this.busy = busy;
     }
 
+    /**
+     * Returns true if busy.
+     * @return busy
+     */
     public boolean isBusy() {
 	return busy;
     }
-
+    
+    /**
+     * Sets status as true.
+     * Will throw exception if already logged in or if pswd does not equals the stored password. 
+     * @param pswd Password
+     * @throws LoginException 
+     */
     public void login(String pswd) throws LoginException {
 	if (status) {
 	    throw new LoginException("Allready logged in!");
