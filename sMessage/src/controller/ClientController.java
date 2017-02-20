ef3f87c089f7a005b1c81f824f59c23b2055cddc
Stian Stensli <stian.hs@hotmail.com>
2017-02-20 14:10:36 +0100

    status funker :)

--- sMessage/src/controller/ClientController.java
+++ sMessage/src/controller/ClientController.java
@@ -3,18 +3,13 @@ package controller;
 import java.io.IOException;
 import java.net.URL;
 import java.util.*;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javafx.scene.control.*;
 import javafx.collections.*;
 import javafx.beans.property.SimpleObjectProperty;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.input.KeyEvent;
-import javafx.stage.Stage;
-import javafx.stage.WindowEvent;
 import model.Client;
 import model.client.*;
 
@@ -24,13 +19,15 @@ import model.client.*;
  * @author s305046, s305080, s305084, s305089
  */
 public class ClientController implements Initializable {
-    
+
     @FXML
     private TableView tvFriends;
     @FXML
     private TableView tvUsers;
     @FXML
     private TableColumn<ClientUser, String> columnUsername;
+    @FXML
+    private TableColumn<ClientUser, String> columnUsernameStatus;
     @FXML
     private TableColumn<Conversation, String> columnFriends;
     @FXML
@@ -41,10 +38,10 @@ public class ClientController implements Initializable {
     private TextArea txtAreaMessages;
     @FXML
     private TextArea txtAreaNewMessage;
-    
+
     private final ObservableList<Conversation> friendList = FXCollections.observableList(new ArrayList<>());
     private final ObservableList<ClientUser> userList = FXCollections.observableList(new ArrayList<>());
-    
+
     private Client client;
     private Conversation activeConversation;
     private boolean removing = false;
@@ -56,16 +53,18 @@ public class ClientController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         initTabel();
     }
-    
+
     private void initTabel() {
         columnUsername.setCellValueFactory((TableColumn.CellDataFeatures<ClientUser, String> param)
                 -> new SimpleObjectProperty<>(param.getValue().getUserName()));
         columnFriends.setCellValueFactory((TableColumn.CellDataFeatures<Conversation, String> param)
                 -> new SimpleObjectProperty<>(param.getValue().getTalkingWithUsername()));
-        
+
+        columnUsernameStatus.setCellValueFactory((TableColumn.CellDataFeatures<ClientUser, String> param)
+                -> new SimpleObjectProperty<>(param.getValue().getStatus().toString()));
         tvFriends.setItems(friendList);
         tvUsers.setItems(userList);
-        
+
         tvFriends.getSelectionModel().getSelectedItems().addListener((ListChangeListener.Change c) -> {
             if (!removing) {
                 int idx = tvFriends.getSelectionModel().getFocusedIndex();
@@ -73,21 +72,27 @@ public class ClientController implements Initializable {
                 setActiveConversation(conv);
             }
         });
-        
+
         tvUsers.getSelectionModel().getSelectedItems().addListener((ListChangeListener.Change c) -> {
-            if (!removing) {
-                int idx = tvUsers.getSelectionModel().getFocusedIndex();
-                if (idx == -1) {
-                    return;
-                }
-                ClientUser user = userList.get(idx);
-                
+            sendRequest();
+        });
+
+    }
+
+    private void sendRequest() {
+        if (!removing) {
+            int idx = tvUsers.getSelectionModel().getFocusedIndex();
+            if (idx == -1) {
+                return;
+            }
+            ClientUser user = userList.get(idx);
+            if (user.getStatus() == Status.ONLINE) {
                 Alert alert = new Alert(AlertType.CONFIRMATION);
                 alert.setTitle("Confirm Connection");
                 alert.setHeaderText("Do you want to connect with " + user.getUserName());
                 alert.setContentText("We will alert you when your peer "
                         + "has responded to the request.");
-                
+
                 Optional<ButtonType> answer = alert.showAndWait();
                 if (answer.isPresent() && answer.get() == ButtonType.OK) {
                     try {
@@ -96,27 +101,32 @@ public class ClientController implements Initializable {
                         showAlertIOException(ex);
                     }
                 }
+            }else if(user.getStatus() == Status.BUSY){
+                Alert alert = new Alert(AlertType.CONFIRMATION);
+                alert.setTitle("Connection Request");
+                alert.setHeaderText("User " + user.getUserName()+" is busy.");
+                alert.setContentText("This person is not available at the moment.");
+
             }
-        });
-        
+        }
     }
-    
+
     private void showAlertIOException(IOException ex) {
         Alert alertErr = new Alert(AlertType.ERROR);
         alertErr.setTitle("Error occurred");
         alertErr.setHeaderText("An IOException occurred");
-        
+
         TextArea txtArea = new TextArea(ex.toString());
         alertErr.getDialogPane().setExpandableContent(txtArea);
     }
-    
+
     private void showAlertBoxError(String title, String contentText) {
         Alert alert = new Alert(AlertType.ERROR);
         alert.setTitle(title);
         alert.setContentText(contentText);
         alert.showAndWait();
     }
-    
+
     public void addMessageToConversation(String userName, Message msg) {
         for (Conversation cnv : friendList) {
             if (cnv.getTalkingWithUsername().equals(userName)) {
@@ -129,11 +139,11 @@ public class ClientController implements Initializable {
                 return;
             }
         }
-        
+
         showAlertBoxError("Error occurred",
                 "This should not happen. Server should have control over this.");
     }
-    
+
     public void updateUserList(String restOfArray) {
         if (restOfArray.equals("")) {
             return;
@@ -143,16 +153,16 @@ public class ClientController implements Initializable {
             userList.add(new ClientUser(users[i], users[i + 1]));
         }
     }
-    
+
     public void showError(String restOfArray) {
         showAlertBoxError("Server error", restOfArray);
     }
-    
+
     public void updateStatus(String username, String status) {
         ClientUser user = new ClientUser(username, status);
         Conversation con = new Conversation(user);
         Status newStatus;
-        
+
         switch (status) {
             case "0":
                 newStatus = Status.OFFLINE;
@@ -167,7 +177,7 @@ public class ClientController implements Initializable {
                 showError("Your client may be outdated.");
                 return;
         }
-        
+
         if (userList.contains(user)) {
             userList.get(userList.indexOf(user)).setStatus(newStatus);
         } else if (friendList.contains(con)) {
@@ -180,6 +190,8 @@ public class ClientController implements Initializable {
         } else {
             userList.add(user);
         }
+        tvUsers.refresh();
+        tvFriends.refresh();
     }
 
     /**
@@ -188,13 +200,13 @@ public class ClientController implements Initializable {
      * @param username
      */
     public void connectRequest(String username) {
-        
+
         ButtonType accept = new ButtonType("Accept", ButtonBar.ButtonData.OK_DONE);
         ButtonType reject = new ButtonType("Reject", ButtonBar.ButtonData.CANCEL_CLOSE);
         Alert alert = new Alert(AlertType.CONFIRMATION, "", accept, reject);
         alert.setTitle("Confirm Connection");
         alert.setHeaderText(username + " wants to connect to you.");
-        
+
         Optional<ButtonType> answer = alert.showAndWait();
         if (answer.isPresent()) {
             try {
@@ -208,14 +220,14 @@ public class ClientController implements Initializable {
             }
         }
     }
-    
+
     public void moveFromUsersToFriends(String username, boolean showAlert) {
         removing = true;
         for (int i = 0; i < userList.size(); i++) {
             if (userList.get(i).getUserName().equals(username)) {
                 friendList.add(new Conversation(userList.get(i)));
                 userList.remove(i);
-                
+
                 break;
             }
         }
@@ -225,7 +237,7 @@ public class ClientController implements Initializable {
             setActiveConversation(null);
         }
         removing = false;
-        
+
         if (showAlert) {
             Alert alert = new Alert(AlertType.INFORMATION);
             alert.setTitle("Accepted");
@@ -233,7 +245,7 @@ public class ClientController implements Initializable {
             alert.show();
         }
     }
-    
+
     public void moveFromFriendsToUser(String username, boolean showAlert) {
         removing = true;
         for (int i = 0; i < friendList.size(); i++) {
@@ -254,16 +266,16 @@ public class ClientController implements Initializable {
             alert.setContentText(username + " disconnected, and has been moved to the user list.");
             alert.show();
         }
-        
+
     }
-    
+
     public void negativeResponse(String username) {
         Alert alert = new Alert(AlertType.INFORMATION);
         alert.setTitle("Rejected");
         alert.setContentText(username + " doesn't want to talk with you...");
-        
+
     }
-    
+
     public void setClient(Client client) {
         this.client = client;
         try {
@@ -272,11 +284,11 @@ public class ClientController implements Initializable {
             e.printStackTrace();
         }
     }
-    
+
     public Client getClient() {
         return client;
     }
-    
+
     private void setActiveConversation(Conversation conv) {
         if (conv != null) {
             activeConversation = conv;
@@ -286,23 +298,23 @@ public class ClientController implements Initializable {
             activeConversation = null;
             labelTalkingWIth.setText("Talking with:");
             txtAreaMessages.clear();
-            
+
         }
     }
-    
+
     private void appendMsgToConversation() {
-        
+
         txtAreaMessages.clear();
         for (Message msg : activeConversation.getMessages()) {
             txtAreaMessages.appendText(msg.toString().replace("&#92", "\n").replace("&#59", ";"));
             txtAreaMessages.appendText("\n");
         }
     }
-    
+
     public void setLeftLabelTest(String text) {
         labelLeftStatus.setText("Your username: " + text);
     }
-    
+
     @FXML
     private void handleSendMsg() {
         try {
@@ -317,7 +329,7 @@ public class ClientController implements Initializable {
             showAlertIOException(ex);
         }
     }
-    
+
     @FXML
     private void handleDisconnectFromUser() {
         try {
@@ -330,7 +342,7 @@ public class ClientController implements Initializable {
             showAlertIOException(ex);
         }
     }
-    
+
     @FXML
     public void buttonPressed(KeyEvent e) {
         if (e.isShiftDown() && e.getCode().toString().equals("ENTER")) {
